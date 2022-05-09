c3324b07046cf5c7b3f2fc2464d2e797849658f6
XiaoYu Chen <chenxia3@os1.engr.oregonstate.edu>
2022-05-08 23:35:37 -0700

    implement exit cmd

--- smallsh.c
+++ smallsh.c
@@ -12,14 +12,14 @@
 #define MAX_LENGTH 2048
 #define MAX_ARG 512
 
-struct movie
-{
-    char *title;
-    int year;
-    char *languages;
-    double rating;
-    struct movie *next;
-}; 
+// struct movie
+// {
+//     char *title;
+//     int year;
+//     char *languages;
+//     double rating;
+//     struct movie *next;
+// }; 
 
 // /* Get user input and tokenize the arguments. Store the input and output file names. */
 // void getInput(char* arguments, char* inputFile, char* outputFile, int* background) {
@@ -32,22 +32,11 @@ struct movie
 //     printf("%s", buf);
 // }
 
-void parse_command() {
-    char line[MAX_LENGTH];
-    int current, index = 0;
-
-    do{
-        current = getchar();
-        line[index] = current;
-        index += 1;
-    } while(index < MAX_LENGTH && current != '\n');
-
-    line[index] = 0;
-    printf("%s", line);
-}
+int parse_command();
 
 /* Prompts the user, then run the command. */
 int main() {
+    int run_flag = 1;
     pid_t pid = getpid();
     char *pidstr;
     {
@@ -59,11 +48,32 @@ int main() {
     printf("pid is:    %d\n", pid);
     printf("pidstr is: %s\n", pidstr);
 
-    for(int i = 0; i < 3; i++) {
+    while (run_flag == 1) {
         printf(": ");
         fflush(stdout);
-        parse_command();
+        run_flag = parse_command();
     }
 
     return EXIT_SUCCESS;
+}
+
+int parse_command() {
+    char line[MAX_LENGTH];
+    int current, index = 0;
+
+    do{
+        current = getchar();
+        line[index] = current;
+        index += 1;
+    } while(index < MAX_LENGTH && current != '\n');
+
+    line[index - 1] = 0;
+    printf("%s", line);
+
+    if (strcmp(line, "exit") == 0) {
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
